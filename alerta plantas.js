107153224234e3958cdaf1a8490179ace32a3bba
jordi1094 <63929965+jordi1094@users.noreply.github.com>
2023-09-11 13:04:42 +0200

    arlama + envio por mail

--- alerta plantas.js
+++ alerta plantas.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
     auth: {
       // TODO: replace `user` and `pass` values from <https://forwardemail.net>
       user: 'tenemossedplantas@gmail.com',
-      pass: 'hdqkmhkshbxsyeka'
+      pass: 'hawdpqeqqswmukom'
     }
   });
 
@@ -20,40 +20,43 @@ const transporter = nodemailer.createTransport({
 //input del sensor. si es 0(false) no pasa corrientet, la tierra esta seca. si es 1 (true) si pasa corriente la tierra esta humeda.
 
 //al no tener el sensor simulamos el inputt generando booleanos aleatorios.
-
-
-//prueva de varias plantas
-var planta1 = false
-var Planta2 = true
-var planta3 = false
-var planta4 = true
-
-if (planta1 || planta2 || planta3 || planta4 === false){
-
-const info = transporter.sendMail({
-    from: '"Tenemos sed" <tenemossedplantas>', // sender address
-    to: "jordi10111994@gmail.com", // list of receivers
-    subject: "Tus plantas necesitasn agua", // Subject line
-    text: "Tengo sed", // plain text body
-    html: <p> las</p> , // html body
-  });
-}
+var humedad = false //Boolean(Math.round(Math.random()));
 // a partir de aqui funciona
-/*
-var humedad = false /*Boolean(Math.round(Math.random()));
+function sendMail(){
+  if (humedad === false) {
+      const info = transporter.sendMail({
+          from: '"Tenemos sed" <tenemossedplantas>', // sender address
+          to: "jordi10111994@gmail.com", // list of receivers
+          subject: "Tus plantas necesitasn agua", // Subject line
+          text: "Tengo sed 2", // plain text body
+          html: "<b>rieganos 5</b>", // html body
+        });
+  }
+  else{
+      console.log('no hay que regar');
+  }
+};
 
+//activación por hora 
+function Alarma(){
+  var hoy = new Date() ;
+  var hora = hoy.getHours();
+  var minuto = hoy.getMinutes();
+  var segundo = hoy.getSeconds();
 
-if (humedad === false) {
-    const info = transporter.sendMail({
-        from: '"Tenemos sed" <tenemossedplantas>', // sender address
-        to: "jordi10111994@gmail.com", // list of receivers
-        subject: "Tus plantas necesitasn agua", // Subject line
-        text: "Tengo sed 2", // plain text body
-        html: "<b>rieganos 3</b>", // html body
-      });
+  if (minuto < 10){
+    minuto = "0" + minuto
+  }
+
+  if(segundo < 10){
+    segundo = "0" + segundo
+  }
+
+  var A_Hora = 13;
+  var A_minutos = 04;
+
+  if (hora == A_Hora && minuto == A_minutos && segundo=="00"){
+    sendMail();
+  }
 }
-else{
-    console.log('no hay que regar')
-}
-*/
-
+setInterval(Alarma,1000)
