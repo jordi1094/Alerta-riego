43a3aa7bc06989daa2ff67ab1e8e74ac98e66a0b
jordi1094 <63929965+jordi1094@users.noreply.github.com>
2025-04-22 21:02:45 +0200

    create testDB and registerUserTest.ts

--- api/src/config/testdb.ts
+++ api/src/config/testdb.ts
@@ -2,15 +2,15 @@ import 'dotenv/config'
 import postgres from 'postgres'
 
 
-if (!process.env.DATABASE_URL) {
+if (!process.env.DATABASE_URL_TEST) {
     throw new Error('DATABASE_URL is not defined in the environment variables')
 }
 
-const connectionString:string = process.env.DATABASE_URL
+const connectionString:string = process.env.DATABASE_URL_TEST
 
-const sql = postgres(connectionString,
+const sqlTest = postgres(connectionString,
     {
         ssl: 'require'
     })
 
-export default sql
+export default sqlTest
