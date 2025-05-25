f667a933c9bf7fda9e98285969f610bc6756e66d
jordi1094 <63929965+jordi1094@users.noreply.github.com>
2025-05-25 21:31:01 +0200

    api register user with postgres working

--- api/src/db/startDb.ts
+++ api/src/db/startDb.ts
@@ -2,15 +2,13 @@ import { Sql } from "postgres";
 
 async function startDb(db:Sql) {
     try {
-        db `CREATE TABLE IF NOT EXISTS users(
+        await db `CREATE TABLE IF NOT EXISTS users(
             id SERIAL PRIMARY KEY,
             name VARCHAR(50),
             username VARCHAR(50) UNIQUE,
             email VARCHAR(100) UNIQUE,
             password VARCHAR(70)
             )`
-
-            console.log("table created")
         
     } catch (error) {
         console.log(error)
